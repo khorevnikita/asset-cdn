3cc12e25a57ffc99a8316138dc93b49b180ccab0
NikKho <nh@codenrock.com>
2021-09-06 17:37:28 +0300

    fix helper

--- src/helpers.php
+++ src/helpers.php
@@ -3,36 +3,36 @@
 use Illuminate\Support\HtmlString;
 use Illuminate\Support\Str;
 
-if (! function_exists('mix_cdn')) {
+if (!function_exists('mix_cdn')) {
     /**
      * Get the path to a versioned Mix file.
      *
-     * @param  string  $path
-     * @param  string  $manifestDirectory
+     * @param string $path
+     * @param string $manifestDirectory
      * @return \Illuminate\Support\HtmlString
      *
      * @throws \Exception
      */
     function mix_cdn($path, $manifestDirectory = '')
     {
-        if (! config('asset-cdn.use_cdn')) {
+        if (!config('asset-cdn.use_cdn')) {
             return mix($path, $manifestDirectory);
         }
 
         static $manifests = [];
 
-        if (! Str::startsWith($path, '/')) {
+        if (!Str::startsWith($path, '/')) {
             $path = "/{$path}";
         }
 
-        if ($manifestDirectory && ! Str::startsWith($manifestDirectory, '/')) {
+        if ($manifestDirectory && !Str::startsWith($manifestDirectory, '/')) {
             $manifestDirectory = "/{$manifestDirectory}";
         }
 
-        $manifestPath = public_path($manifestDirectory.'/mix-manifest.json');
+        $manifestPath = public_path($manifestDirectory . '/mix-manifest.json');
 
-        if (! isset($manifests[$manifestPath])) {
-            if (! file_exists($manifestPath)) {
+        if (!isset($manifests[$manifestPath])) {
+            if (!file_exists($manifestPath)) {
                 throw new Exception('The Mix manifest does not exist.');
             }
 
@@ -41,36 +41,40 @@ if (! function_exists('mix_cdn')) {
 
         $manifest = $manifests[$manifestPath];
 
-        if (! isset($manifest[$path])) {
+        if (!isset($manifest[$path])) {
             throw new Exception("Unable to locate Mix file: {$path}.");
         }
 
-        $cdnUrl = config('asset-cdn.cdn_url');
+        $folder = config("asset-cdn.cdn_folder");
+        $endpoint = config('asset-cdn.cdn_url');
+        $cdnUrl = "$endpoint/$folder";
         // Remove slashes from ending of the path
         $cdnUrl = rtrim($cdnUrl, '/');
 
-        return new HtmlString($cdnUrl.$manifestDirectory.$manifest[$path]);
+        return new HtmlString($cdnUrl . $manifestDirectory . $manifest[$path]);
     }
 }
 
-if (! function_exists('asset_cdn')) {
+if (!function_exists('asset_cdn')) {
 
     /**
      * Generate an asset path for the application.
      *
-     * @param  string  $path
+     * @param string $path
      * @return string
      */
     function asset_cdn($path)
     {
-        if (! config('asset-cdn.use_cdn')) {
+        if (!config('asset-cdn.use_cdn')) {
             return asset($path);
         }
 
-        $cdnUrl = config('asset-cdn.cdn_url');
+        $folder = config("asset-cdn.cdn_folder");
+        $endpoint = config('asset-cdn.cdn_url');
+        $cdnUrl = "$endpoint/$folder";
         // Remove slashes from ending of the path
         $cdnUrl = rtrim($cdnUrl, '/');
 
-        return $cdnUrl.'/'.trim($path, '/');
+        return $cdnUrl . '/' . trim($path, '/');
     }
 }
